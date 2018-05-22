639924cdb7481bcfd15b002456d06183ebbbb5b0
chengxueying <cxy_lucky@163.com>
2018-05-22 09:33:29 +0800

    Correct Task0 answer.

--- investigate texts and calls/ZH/Task0.py
+++ investigate texts and calls/ZH/Task0.py
@@ -20,4 +20,4 @@ with open('calls.csv', 'r') as f:
 """
 
 print ("First record of texts,{} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
-print ("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[0][0], calls[0][1], calls[0][2],calls[0][3]))
+print ("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[-1][0], calls[-1][1], calls[-1][2],calls[-1][3]))
